4009c69ad6b51b476c4b6afb29b40075a48df7a5
menoo-o <menosuper6@gmail.com>
2024-11-12 10:24:30 +0500

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+export default function about() {
+  return (
+    <>
+      <h1>this is about pg</h1>
+
+     <br />
+     <br />
+     <Link href='/'>back to home</Link>
+    </>
+  )
+}
